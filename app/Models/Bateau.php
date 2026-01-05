124afa70510d5f5e0d6accd14b6cfdaecb92185e
palatete <yanisprime@hotmail.fr>
2026-01-05 13:39:22 +0100

    Add controllers and relationships for Bateau and Equipement
    Created BateauController and EquipementController. Added hasMany relationship from Bateau to Equipement and belongsTo relationship from Equipement to Bateau in their respective models. Also updated Equipement model to include 'bateau_id' in fillable attributes.

--- app/Models/Bateau.php
+++ app/Models/Bateau.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bateau extends Model
 {
-    //
+    public function equipements(): HasMany
+    {
+        return $this->hasMany(Equipement::Class);
+    }
 }
--- app/Models/Equipement.php
+++ app/Models/Equipement.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Equipement extends Model
 {
     protected $table = 'equipement';
 
     protected $fillable = [
+        'bateau_id',
         'name',
         'description',
     ];
+
+    public function bateau(): BelongsTo
+    {
+        return $this->belongsTo(Bateau::class);
+    }
 }
